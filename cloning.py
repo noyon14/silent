c4adce11188c1c34a6c9137178699b5a6237f9a0
Rakibul Islam Noyon <70952823+noyon14@users.noreply.github.com>
2020-09-25 07:14:23 +0600

    Create cloning.py

---
+++ cloning.py
@@ -0,0 +1,49 @@
+# silent
+![](https://img.shields.io/badge/lovefucker420-SilentForce-orange?style=for-the-badge&logo=python.svg) 
+<p align="center">
+<a href="#"><img title="Made in Bangladesh " src="https://img.shields.io/badge/MADE%20IN-Bangladesh-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<img alt="lovefucker420' Github Stats" src="https://github-readme-stats.vercel.app/api?username=Noyon&show_icons=true&include_all_commits=true&hide_border=true" />
+<!--  <img alt="profile pic" width="195px" src="https://avatars2.githubusercontent.com/u/26059688?s=460&u=d41b000a62eab50d000c3da604d151cec27bd850&v=4" />  -->
+<!--  <img src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=Noyon&hide=ruby,perl&hide_border=true" />  -->
+</p>
+<p align="center">
+<a href="#"><img title="WhatsApp Num" src="https://img.shields.io/badge/WhatsApp%20Number-+8801305055097-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<p align="center">
+<p align="center">
+<a href="https://github.com/Noyon"><img title="lovefucker420" src="https://img.shields.io/badge/lovefucker420-Noyon-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/lovefucker420/sf/followers"><img title="Followers" src="https://img.shields.io/github/followers/lovefucker420?color=blue&style=flat-square"></a>
+<a href="https://github.com/lovefucker420/sf/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/lovefucker420/sf?color=red&style=flat-square"></a>
+<a href="https://github.com/Noyon/sf/network/members"><img title="Forks" src="https://img.shields.io/github/forks/lovefucker420/sf?color=red&style=flat-square"></a>
+<a href="https://github.com/Noyon/sf/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/lovefucker420/sf?label=Watchers&color=blue&style=flat-square"></a>
+</p>
+
+# Installation :
+$ `apt update`
+
+$ `apt upgrade -y`
+
+$ `apt install git -y`
+
+$ `pkg install python`
+
+$ `pkg install python2 -y`
+
+$ `pip2 install requests`
+
+$ `pip2 install mechanize`
+
+$ `pip install lolcat`
+
+$ `git clone https://github.com/lovefucker420/sf`
+
+$ `cd sf`
+
+$ `python2 sfteam.py`
+
+
